3526ad79cfb05f1911528912bc962daba2009df9
Abdo-Walid12358 <totowalid2000@gmail.com>
2024-02-12 12:50:59 +0200

    Updated

--- app/posts/[postID]/page.jsx
+++ app/posts/[postID]/page.jsx
@@ -2,13 +2,33 @@ import Header from "@/components/header/header";
 import PostDetails from "@/components/postDetails/postDetails";
 import Footer from "@/components/footer/footer";
 
-export const metadata = {
-    title: "Post",
+async function getTitleProduct(postID) {
+    try {
+        const res = await fetch(`https://dummyjson.com/products/${postID}`);
+        const postsData = await res.json();
+
+        if(postsData.title !== undefined){
+            return postsData.title;
+        }else{
+            return "Page Not Found";
+        }
+    } catch (err) {
+        return "Page Not Found";
+    }
+}
+
+export const generateMetadata = async ({ params }) => {
+    const postID = params.postID;
+    const titlePage = await getTitleProduct(postID);
+
+    return {
+        title: `${titlePage}`,
+    };
 };
 
 export default function Main({ params }) {
     const postID = params.postID;
-    
+
     return (
         <>
             <Header />
