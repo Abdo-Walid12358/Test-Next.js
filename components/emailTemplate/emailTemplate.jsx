0afdb306d869dd75a610832d1e61863218fe51ef
Abdo-Walid12358 <totowalid2000@gmail.com>
2024-02-13 21:07:49 +0200

    update

--- components/emailTemplate/emailTemplate.jsx
+++ components/emailTemplate/emailTemplate.jsx
@@ -1,9 +1,16 @@
-//import "./.emailTemplate.css"
-
 export const EmailTemplate = ({ username, email, message }) => (
-    <div>
-        <h1>Username : {username}</h1>
-        <h2>Email : {email}</h2>
+    <div
+        className="email-box"
+        style={{
+            background: "deeppink",
+            padding: "10px 15px",
+            border: "2px solid black",
+            borderRadius: "5px",
+            color: "white"
+        }}
+    >
+        <h3 style={{marginBottom: "5px"}}>Username : {username}</h3>
+        <h4 style={{marginBottom: "5px"}}>Email : {email}</h4>
         <p>Message : {message}</p>
     </div>
 );
